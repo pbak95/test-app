b178242dd830a235211b3e3f10443d7c712481ee
pbak_c <patryk.bak@bluesoft.com>
2022-02-25 15:49:07 +0100

    add sample spring boot app in kotlin

---
+++ src/main/kotlin/pl/pbak/demo/test/TestController.kt
@@ -0,0 +1,30 @@
+package pl.pbak.demo.test
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import lombok.RequiredArgsConstructor
+import lombok.extern.slf4j.Slf4j
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/test")
+class TestController {
+
+    @GetMapping(value = ["/uuid"], produces = ["application/json"])
+    @Operation(
+            operationId = "generateRandomUuid",
+            summary = "Generates random UUID",
+            tags = ["auth"])
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "201", description = "new UUID correlationId with signature was generated"),
+    ])
+    fun generateSignedCorrelationId(): UUID {
+        return UUID.randomUUID();
+    }
+}
